4d6c84e7ac8acf8a886457259f0863b23a7ee029
Charles Shang <creyslz@gmail.com>
2015-10-17 14:54:15 -0500

    Added some comment sudocode
    uh, yeah

--- pictur/views.py
+++ pictur/views.py
@@ -1,19 +1,26 @@
 from pictur import app
 from flask import Flask, url_for, redirect, request, render_template
+import os
+
+UPLOADS_FOLDER = ''
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+	'''images = get_recent_images() #images should be a list of image id's '''
+    return render_template('index.html')#, images = images)
 	
-@app.route('/p/<subpic>/i/<image_id>')
-def profile(subpic, image_id):
-    return render_template('index.html')
+@app.route('/i/<image_id>')
+def profile(image_id):
+	'''image_data = get_image_metadata(image_id) #image_data should be a dictionary of title, description, etc.'''
+    return render_template('index.html')#, image_id = image_id, image_data = image_data)
 
-@app.route('/p/<subpic>/upload', methods=['GET', 'POST'])
-def upload_file(subpic):
-    '''if request.method == 'POST':
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    '''if request.method == 'POST' and UPLOADS_FOLDER is not '':
         file = request.files['file']
-        file.save('C:\\Users\\Charles\\Documents\\411Project\\upload\\test.png')
-        return redirect(url_for('index'))'''
+		image_id = upload_to_database(file) #filename should be an image id
+		filename = image_id + '.png'
+        file.save(os.path.join(UPLOADS_FOLDER, filename))
+        return redirect(url_for('/i/' + image_id))'''
     return render_template('index.html')
