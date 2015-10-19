688d98396a73033c7817504ace9b73dd985be8fc
Pictur Host <creyslz@gmail.com>
2015-10-18 22:59:52 -0400

    searchbar

---
+++ pictur/static/app.js
@@ -0,0 +1,29 @@
+function updateUpload(i) {
+	document.getElementById("fileUpload-text").value = i.value.replace("C:\\fakepath\\", "");
+};
+function showUpload(){
+	var div = $("#upload-section");
+	var modal = $("#modal");
+	if(div.is(":hidden")){
+		div.fadeIn(400);
+		modal.fadeIn(400);
+	} else {
+		div.fadeOut(400);
+		modal.fadeOut(400);
+	}
+}
+function handleSearch(event){
+	if(event.keyCode === 13){
+		console.log($("#search-box"));
+		window.location.href = "http://pictur.ml:2086/search/tags/"+$("#search-box")[0].value;
+	}
+	return false;
+}
+function showReply(i){
+	if($(i).parent().has("div.comment-reply-wrapper").length == 0){
+		$('<div class="comment-reply-wrapper"><form><textarea></textarea><button class="post-button">Post</button></form></div>').insertAfter($(i).parent().children("p.comment-text"));
+	}
+	else{
+		$(i).parent().children("div.comment-reply-wrapper").remove();
+	}
+}
--- runserver.py
+++ runserver.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 from pictur import app
 
-app.run(debug=True, host='0.0.0.0', threaded=True)
+app.run(debug=True, host='0.0.0.0', threaded=True, port=2086)
